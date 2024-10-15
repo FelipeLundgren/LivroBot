01eb78fae9a558ee478e7a644657f32478af28da
FelipeLundgren <felipec5l2@hotmail.com>
2024-10-15 18:26:17 -0300

    G: organizar e printar

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 with open("/home/abubu/workspace/github.com/FelipeLundgren/bookbot/books/frankenstein.txt") as f:
     file_contents = f.read()
 
-print(file_contents)
+
 words = len(file_contents.split())
 
-print(f"total de palavras no texto é de: {words}")
 
-def contador ()
-    count_lower = file_contents.lower()
-    dicionario = {}
-    for i in count_lower:
-        if i in dicionario:
-            dicionario[i] += 1
-        else:
-            dicionario[i] = 1
-    print(dicionario)
+
+
+count_lower = file_contents.lower()
+dicionario = {}
+
+for i in count_lower:
+    if i in dicionario:
+        dicionario[i] += 1
+    else:
+        dicionario[i] = 1
+dicionario_alpha ={}
+for char, count in dicionario.items():
+    if char.isalpha():
+        dicionario_alpha[char] = count
+dicionario_ordem = sorted(dicionario_alpha.items(), key=lambda x: x[1], reverse=True)
+
+print(f"--- Begin report of frankenstein.txt ---")
+print(f"{words} words found in the document")
+
+for char, count in dicionario_ordem:
+    print(f"the '{char}' character was found {count} times")
+
+print("--- End report ---")
+
+
+
+
 
