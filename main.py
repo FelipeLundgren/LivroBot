d747994470ed29222feaa9098b03283b6714d01d
FelipeLundgren <felipec5l2@hotmail.com>
2024-10-15 18:03:40 -0300

    F: contador de caracteres

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ print(file_contents)
 words = len(file_contents.split())
 
 print(f"total de palavras no texto é de: {words}")
+
+def contador ()
+    count_lower = file_contents.lower()
+    dicionario = {}
+    for i in count_lower:
+        if i in dicionario:
+            dicionario[i] += 1
+        else:
+            dicionario[i] = 1
+    print(dicionario)
+
