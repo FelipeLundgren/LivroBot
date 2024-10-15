0c25e5977ebfb0cedd624067507e1639add6acaa
FelipeLundgren <felipec5l2@hotmail.com>
2024-10-15 17:49:12 -0300

    E:contador de palavras

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ with open("/home/abubu/workspace/github.com/FelipeLundgren/bookbot/books/franken
     file_contents = f.read()
 
 print(file_contents)
+words = len(file_contents.split())
+
+print(f"total de palavras no texto é de: {words}")
